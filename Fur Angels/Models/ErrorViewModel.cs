b02cd7a7a901d5fe9f382cc21e43a59fefadf097
Mahek <mahekbabariya18@gmail.com>
2025-03-07 21:50:24 +0530

    Add project files.

---
+++ Fur Angels/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fur_Angels.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
